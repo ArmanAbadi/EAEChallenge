fbff1d538ec14f567314301bb8861379081325a5
ArmanAbadi <arman_abadi@hotmail.com>
2025-01-03 02:52:41 -0800

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore"; 
+const initializeApp = require("firebase/app");
+const getFirestore = require("firebase/firestore");
+const collection = require("firebase/firestore");
+const getDocs = require("firebase/firestore");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
